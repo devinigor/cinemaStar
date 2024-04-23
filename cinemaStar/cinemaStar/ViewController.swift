1be7410953038be6456b06fc7495cfdba3b9ae6c
devinigor <devinigor@icloud.com>
2024-04-23 21:29:15 +0300

    no message

---
+++ cinemaStar/cinemaStar/ViewController.swift
@@ -0,0 +1,11 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
